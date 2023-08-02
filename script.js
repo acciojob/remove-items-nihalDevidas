ebe0953e49de756e86169e89adb7c293603c712b
nihalDevidas <117895185+nihalDevidas@users.noreply.github.com>
2023-08-02 23:39:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+
+let secElement = document.getElementById("colorSelect");
+
+const btn = document.querySelector("form >input");
+
+
+
+btn.addEventListener('click',()=>{
+	let index = secElement.selectedIndex;
+
+	secElement.remove(index);
+	
+});
+
+
